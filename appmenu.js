8e1e092493786d989c3d341262bd742839b61b66
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-02-18 08:36:11 +0300

    changes made

--- appmenu.js
+++ appmenu.js
@@ -105,7 +105,7 @@ const menu = [
     },
 {
         id:14,
-        title:"Coffee",
+        title:"foodiee",
         price:19.89,
         category:"Lunch",
         img:"./Images/food6.jpg",
@@ -116,7 +116,10 @@ const menu = [
 const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded',function(){
-    let displayMenu = menu.map(function(item){
+    displayMenuItems(menu);
+});
+function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function(item){
          
         return ` <article class="menu-item">
             <img src=${item.img} class="photo" alt=${item.title}>
@@ -133,4 +136,4 @@ window.addEventListener('DOMContentLoaded',function(){
     });
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu;
-});
+}

159c232470120b8acb01cbfc6918fe922c0113ed
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-02-18 12:37:23 +0300

    Added some new changes

--- appmenu.js
+++ appmenu.js
@@ -151,7 +151,32 @@ const container = document.querySelector('.btn-container')
 // load items
 window.addEventListener('DOMContentLoaded',function(){
     displayMenuItems(menu);
+    
+    displayMenuButtons();
 
+});
+
+
+function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function(item){
+         
+        return ` <article class="menu-item">
+            <img src=${item.img} class="photo" alt=${item.title}>
+            <div class="item-info">
+                <header>
+               <h4>${item.title}</h4>
+                <h5 class="price">${item.price}</h5>
+                </header>
+                <p class="item-text">
+                   ${item.desc}
+                </p>
+            </div>
+        </article>`;
+    });
+    displayMenu = displayMenu.join("");
+    sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons(){
     const categories = menu.reduce(function(values,item){
         if(!values.includes(item.category)){
             values.push(item.category);
@@ -185,26 +210,4 @@ filterBtns.forEach(function(btn){
     });
 });
 
-
-});
-
-
-function displayMenuItems(menuItems){
-    let displayMenu = menuItems.map(function(item){
-         
-        return ` <article class="menu-item">
-            <img src=${item.img} class="photo" alt=${item.title}>
-            <div class="item-info">
-                <header>
-               <h4>${item.title}</h4>
-                <h5 class="price">${item.price}</h5>
-                </header>
-                <p class="item-text">
-                   ${item.desc}
-                </p>
-            </div>
-        </article>`;
-    });
-    displayMenu = displayMenu.join("");
-    sectionCenter.innerHTML = displayMenu;
 }
